3d2c1e243cf7b31ca9a37817c0bcc69a2537761c
MaximoRdz <mxrdhr@gmail.com>
2024-03-19 20:43:20 +0100

    Searching Exercises

---
+++ lab1-Searching-Heuristics/graphs.py
@@ -0,0 +1,8 @@
+class Neighbor:
+    def __init__(self, node, weight=None) -> None:
+        self.node = node
+        self.weight = weight
+
+
+class Graph:
+    pass
---
+++ lab1-Searching-Heuristics/search_algorithms.py
@@ -0,0 +1,14 @@
+"""
+This script includes some searching algorithms based on
+graph defined by python dictionaries such that each entry
+represents an edge of the graph in the following fashion:
+
+"A": [("B", 2), ("C", 3), ...]
+
+As you can see we'll work with weighted graphs.
+"""
+
+
+def dfs():
+    pass
+
